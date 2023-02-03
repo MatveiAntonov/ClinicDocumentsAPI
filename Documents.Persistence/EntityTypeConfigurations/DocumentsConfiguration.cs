02397aa139ce488761e7bf8d257144127b142b7c
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 13:09:23 +0300

    Added entity configurations

---
+++ Documents.Persistence/EntityTypeConfigurations/DocumentsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Documents.Domain.Entities.EntitiesLocationData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.Persistence.EntityTypeConfigurations
+{
+    public class DocumentsConfiguration : IEntityTypeConfiguration<Document>
+    {
+        public void Configure(EntityTypeBuilder<Document> builder)
+        {
+            builder.HasKey(document => document.Id);
+            builder.HasIndex(document => document.Id).IsUnique();
+            builder.Property(document => document.Name).HasMaxLength(250)
+                .IsRequired();
+            builder.Property(document => document.Url).IsRequired();
+        }
+    }
+}
---
+++ Documents.Persistence/EntityTypeConfigurations/PhotoConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Documents.Domain.Entities.EntitiesLocationData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.Persistence.EntityTypeConfigurations
+{
+    public class PhotoConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.HasKey(photo => photo.Id);
+            builder.HasIndex(photo => photo.Id).IsUnique();
+            builder.Property(photo => photo.Name).HasMaxLength(250)
+                .IsRequired();
+            builder.Property(photo => photo.Url).IsRequired();
+        }
+    }
+}
