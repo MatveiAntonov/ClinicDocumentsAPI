4fda28014d69aeab11f3097535b115ded248a0e2
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 13:08:47 +0300

    Added context for mssql database

---
+++ Documents.Persistence/Contexts/DocumentsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Documents.Domain.Entities.EntitiesLocationData;
+using Documents.Domain.Interfaces.Contexts;
+using Documents.Persistence.EntityTypeConfigurations;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.Emit;
+
+namespace Documents.Persistence.Contexts
+{
+    public class DocumentsDbContext : DbContext, IDocumentsDbContext
+    {
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DocumentsDbContext(DbContextOptions<DocumentsDbContext> options)
+        : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new DocumentsConfiguration());
+            builder.ApplyConfiguration(new PhotoConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
