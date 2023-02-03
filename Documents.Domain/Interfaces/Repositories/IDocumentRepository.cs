7d7c235d04e0205301d274a1af594ed2fc1094e7
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 12:57:37 +0300

    Added repositories interfaces

---
+++ Documents.Domain/Interfaces/Repositories/IDocumentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Documents.Domain.Entities.EntitiesContentData;
+using Documents.Domain.Entities.EntitiesLocationData;
+using Microsoft.AspNetCore.Http;
+
+namespace Documents.Domain.Interfaces.Repositories
+{
+    public interface IDocumentRepository
+    {
+        Task<IEnumerable<Blob>> ListAsync(CancellationToken cancellationToken);
+        Task<Blob?> DownloadAsync(string blobFileName, CancellationToken cancellationToken);
+        Task<BlobResponse> DeleteAsync(string blobFileName, CancellationToken cancellationToken);
+        Task<BlobResponse> UploadAsync(IFormFile file, int ResultId, CancellationToken cancellationToken);
+    }
+}
---
+++ Documents.Domain/Interfaces/Repositories/IPhotoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Documents.Domain.Entities.EntitiesContentData;
+using Documents.Domain.Entities.EntitiesLocationData;
+using Microsoft.AspNetCore.Http;
+
+namespace Documents.Domain.Interfaces.Repositories
+{
+    public interface IPhotoRepository
+    {
+        Task<IEnumerable<Blob>> ListAsync(CancellationToken cancellationToken);
+        Task<Blob?> DownloadAsync(string blobFileName, CancellationToken cancellationToken);
+        Task<BlobResponse> DeleteAsync(string blobFileName, CancellationToken cancellationToken);
+        Task<BlobResponse> UploadAsync(IFormFile file, CancellationToken cancellationToken);
+    }
+}
