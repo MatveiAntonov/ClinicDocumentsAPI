c19fb3c45cd8257b1245b303672754312090089a
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 13:13:53 +0300

    Added View model for files

---
+++ Documents.WebApi/Models/ViewModels/FileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Documents.WebApi.Models.ViewModels
+{
+    public class FileViewModel
+    {
+        public IFormFile? File { get; set; }
+        public int ResultId{ get; set; }
+    }
+}
