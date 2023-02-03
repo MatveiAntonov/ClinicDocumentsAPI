d0dab1ec7891560fc07020b62b65d9dc1805c7b2
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 12:55:55 +0300

    Added entities for mssql service

---
+++ Documents.Domain/Entities/EntitiesLocationData/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.Domain.Entities.EntitiesLocationData
+{
+    public class Document
+    {
+        
+
+        public int Id { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int ResultId { get; set; }
+    }
+}
