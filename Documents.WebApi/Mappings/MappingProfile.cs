07925a35668338dbb1c0b2f7c33abd648512969e
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 13:12:56 +0300

    Added Mapping profile for AutoMapper

---
+++ Documents.WebApi/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Documents.Domain.Entities.EntitiesContentData;
+using Documents.Domain.Entities.EntitiesLocationData;
+using Documents.WebApi.Models.DTOs;
+
+namespace Profiles.WebApi.Mappings; 
+public class MappingProfile : Profile {
+    public MappingProfile()
+    {
+        CreateMap<Blob, BlobDto>().ReverseMap();
+        CreateMap<BlobResponse, BlobResponseDto>().ReverseMap();
+    }
+}
