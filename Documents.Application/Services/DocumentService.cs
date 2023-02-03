9acf3663de0756329948cae428ca42f3925ec443
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 12:58:52 +0300

    Added services

---
+++ Documents.Application/Services/DocumentService.cs
@@ -0,0 +1,60 @@
+﻿using Documents.Domain.Entities;
+using Documents.Domain.Entities.EntitiesContentData;
+using Documents.Domain.Interfaces.Contexts;
+using Documents.Domain.Interfaces.Repositories;
+using Documents.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.Application.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        private readonly IDocumentRepository _documentRepository;
+        private readonly IDocumentsDbContext _documentsDbContext;
+
+        public DocumentService(IDocumentRepository repository, IDocumentsDbContext documentsDbContext)
+        {
+            _documentRepository = repository;
+            _documentsDbContext = documentsDbContext;
+
+        }
+        public Task<IEnumerable<Blob>> ListAsync(CancellationToken cancellationToken)
+        {
+            return _documentRepository.ListAsync(cancellationToken);
+        }
+
+        public async Task<Blob?> DownloadAsync(int id, CancellationToken cancellationToken)
+        {
+            var doc = await _documentsDbContext.Documents.FindAsync(id);
+
+            if (doc is not null)
+            {
+                return await _documentRepository.DownloadAsync(doc.Name, cancellationToken);
+            }
+            return null;
+        }
+
+        public async Task<BlobResponse> DeleteAsync(int id, CancellationToken cancellationToken)
+        {
+            var doc = await _documentsDbContext.Documents.FindAsync(id);
+
+            if (doc is not null)
+            {
+                return await _documentRepository.DeleteAsync(doc.Name, cancellationToken);
+            }
+            return null;
+        }
+
+        public async Task<BlobResponse> UploadAsync(IFormFile file, int resultId, CancellationToken cancellationToken)
+        {
+            return await _documentRepository.UploadAsync(file, resultId, cancellationToken);
+        }
+    }
+}
---
+++ Documents.Application/Services/PhotoService.cs
@@ -0,0 +1,53 @@
+﻿using Documents.Domain.Entities.EntitiesContentData;
+using Documents.Domain.Interfaces.Contexts;
+using Documents.Domain.Interfaces.Repositories;
+using Documents.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace Documents.Application.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly IPhotoRepository _documentRepository;
+        private readonly IDocumentsDbContext _documentsDbContext;
+
+        public PhotoService(IPhotoRepository repository, IDocumentsDbContext documentsDbContext)
+        {
+            _documentRepository = repository;
+            _documentsDbContext = documentsDbContext;
+
+        }
+        public Task<IEnumerable<Blob>> ListAsync(CancellationToken cancellationToken)
+        {
+            return _documentRepository.ListAsync(cancellationToken);
+        }
+
+        public async Task<Blob?> DownloadAsync(int id, CancellationToken cancellationToken)
+        {
+            var doc = await _documentsDbContext.Photos.FindAsync(id);
+
+            if (doc is not null)
+            {
+                return await _documentRepository.DownloadAsync(doc.Name, cancellationToken);
+            }
+            return null;
+        }
+
+        public async Task<BlobResponse> DeleteAsync(int id, CancellationToken cancellationToken)
+        {
+            var doc = await _documentsDbContext.Photos.FindAsync(id);
+
+            if (doc is not null)
+            {
+                return await _documentRepository.DeleteAsync(doc.Name, cancellationToken);
+            }
+            return null;
+        }
+
+        public async Task<BlobResponse> UploadAsync(IFormFile file, CancellationToken cancellationToken)
+        {
+            return await _documentRepository.UploadAsync(file, cancellationToken);
+        }
+    }
+}
