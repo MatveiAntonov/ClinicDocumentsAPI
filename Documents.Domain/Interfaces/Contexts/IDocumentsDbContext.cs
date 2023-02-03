38c4d6091b6b7c65c8a7664a408a4827d8013572
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 12:57:21 +0300

    Added interface for context

---
+++ Documents.Domain/Interfaces/Contexts/IDocumentsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Documents.Domain.Entities.EntitiesLocationData;
+using Microsoft.EntityFrameworkCore;
+
+namespace Documents.Domain.Interfaces.Contexts
+{
+    public interface IDocumentsDbContext
+    {
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
