86b106716bb62850524d156a7bc12f8d5c7dd675
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 12:55:10 +0300

    Added data for blobservice

---
+++ Documents.Domain/Entities/EntitiesContentData/BlobResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.Domain.Entities.EntitiesContentData
+{
+    public class BlobResponse
+    {
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public Blob? Blob { get; set; } = new();
+    }
+}
