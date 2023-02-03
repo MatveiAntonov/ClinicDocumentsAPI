aa943130a08578f5ccb649f0c53f8ee03ecc124a
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 12:58:04 +0300

    Added interfaces for services

---
+++ Documents.Domain/Interfaces/Services/IPhotoService.cs
@@ -0,0 +1,14 @@
+﻿using Documents.Domain.Entities.EntitiesContentData;
+using Documents.Domain.Entities.EntitiesLocationData;
+using Microsoft.AspNetCore.Http;
+
+namespace Documents.Domain.Interfaces.Services
+{
+    public interface IPhotoService
+    {
+        Task<IEnumerable<Blob>> ListAsync(CancellationToken cancellationToken);
+        Task<Blob?> DownloadAsync(int id, CancellationToken cancellationToken);
+        Task<BlobResponse> DeleteAsync(int id, CancellationToken cancellationToken);
+        Task<BlobResponse> UploadAsync(IFormFile file, CancellationToken cancellationToken);
+    }
+}
