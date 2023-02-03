d0a797cf7171851e1b97dbc7432363b79e02d135
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 13:13:15 +0300

    DTOs for controllers

---
+++ Documents.WebApi/Models/DTOs/BlobResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using Documents.Domain.Entities.EntitiesContentData;
+
+namespace Documents.WebApi.Models.DTOs
+{
+    public class BlobResponseDto
+    {
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public BlobDto? Blob { get; set; } = new();
+    }
+}
