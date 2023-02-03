8fa78b2b6530e515ff1dba9232fdcfc1961ebe46
ProsTuha <tuhalf2002@mail.ru>
2023-02-03 13:09:55 +0300

    Added DI in Documents.Persistence

---
+++ Documents.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Documents.Domain.Interfaces.Contexts;
+using Documents.Domain.Interfaces.Repositories;
+using Documents.Persistence.Contexts;
+using Documents.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Documents.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection ConnectDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<DocumentsDbContext>(options => options
+            .UseSqlServer(connectionString));
+
+            services.AddScoped<IDocumentsDbContext, DocumentsDbContext>();
+
+            services.AddScoped<IDocumentRepository, DocumentRepository>();
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+            return services;
+        }
+    }
+}
