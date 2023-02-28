e7c40c0af2e0bdad24efff9293bc402aebb61928
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 11:03:17 +0300

    Added photo messages for consumer

---
+++ Documents.Application/Consumer/Messages/Photos/PhotoAdded.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Events
+{
+	public class PhotoAdded
+	{
+		public string PhotoName { get; set; } = String.Empty;
+		public byte[] PhotoData { get; set; }
+	}
+}
