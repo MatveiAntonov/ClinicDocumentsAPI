c4f407bfc7698cf3d9525d77ae02df599f4b6a76
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 11:25:09 +0300

    Added PhotoDto model as a recieved type for Upload method in repository

---
+++ Documents.Domain/DTOs/Photos/PhotoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.Domain.DTOs.Photos
+{
+	public class PhotoDto
+	{
+		public string PhotoName { get; set; } = String.Empty;
+		public byte[] PhotoData { get; set; }
+	}
+}
