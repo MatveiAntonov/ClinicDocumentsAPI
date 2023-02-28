06568d24d1ffc15469897ea2c35a5f87c3ddfaf7
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 11:26:30 +0300

    Added Id field to Blob class

--- Documents.Domain/Entities/EntitiesContentData/Blob.cs
+++ Documents.Domain/Entities/EntitiesContentData/Blob.cs
@@ -8,6 +8,7 @@ namespace Documents.Domain.Entities.EntitiesContentData
 {
     public class Blob
     {
+        public int Id { get; set; }
         public string? Uri { get; set; }
         public string? Name { get; set; }
         public string? ContentType { get; set; }
