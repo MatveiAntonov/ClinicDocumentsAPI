6750fb9c4c16dc5b50454a2c1e052fdd3800afc3
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 11:01:30 +0300

    Added events for photo consumer

---
+++ Documents.Application/Consumer/Events/Photos/PhotoAddedConsumer.cs
@@ -0,0 +1,37 @@
+﻿using Documents.Domain.DTOs.Photos;
+using Documents.Domain.Interfaces.Repositories;
+using Events;
+using MassTransit;
+
+namespace Documents.Application.Consumer.Events.Photos
+{
+	public class PhotoAddedConsumer : IConsumer<PhotoAdded>
+	{
+		private readonly IPhotoRepository _photoRepository;
+
+		public PhotoAddedConsumer(IPhotoRepository photoRepository)
+		{
+			_photoRepository = photoRepository;
+		}
+
+		public async Task Consume(ConsumeContext<PhotoAdded> context)
+		{
+			if (context.Message is not null)
+			{
+				var photoDto = new PhotoDto
+				{
+					PhotoData = context.Message.PhotoData,
+					PhotoName = context.Message.PhotoName
+				};
+				var result = await _photoRepository.UploadAsync(photoDto, default(CancellationToken));
+				await context.RespondAsync(new
+				{
+					Id = result.Blob.Id,
+					PhotoUrl = result.Blob.Uri,
+					PhotoName = result.Blob.Name
+				});
+			}
+
+		}
+	}
+}
---
+++ Documents.Application/Consumer/Events/Photos/PhotoDeletedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Documents.Domain.Interfaces.Repositories;
+using Events;
+using MassTransit;
+
+namespace Documents.Application.Consumer.Events.Photos
+{
+	public class PhotoDeletedConsumer : IConsumer<PhotoDeleted>
+	{
+		private readonly IPhotoRepository _photoRepository;
+
+		public PhotoDeletedConsumer(IPhotoRepository photoRepository)
+		{
+			_photoRepository = photoRepository;
+		}
+
+		public async Task Consume(ConsumeContext<PhotoDeleted> context)
+		{
+			if (context.Message is not null)
+			{
+				var result = await _photoRepository.DeleteAsync(context.Message.PhotoName, default(CancellationToken));
+			}
+
+		}
+	}
+}
