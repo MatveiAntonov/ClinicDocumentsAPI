a60cbe4d5672278cc4bdff1f80af446a32e248d4
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 18:40:49 +0300

    Added consumer for result entity

---
+++ Documents.Application/Consumer/Events/Results/ResultCreatedConsumer.cs
@@ -0,0 +1,31 @@
+﻿using Documents.Domain.DTOs;
+using Documents.Domain.Interfaces.Repositories;
+using Events;
+using MassTransit;
+
+namespace Documents.Application.Consumer.Events.Results
+{
+	public class ResultCreatedConsumer : IConsumer<ResultCreated>
+	{
+		private readonly IDocumentRepository _documentRepository;
+
+		public ResultCreatedConsumer(IDocumentRepository documentRepository)
+		{
+			_documentRepository = documentRepository;
+		}
+
+		public async Task Consume(ConsumeContext<ResultCreated> context)
+		{
+			if (context.Message is not null)
+			{
+				var documentDto = new ResultDto
+				{
+					Id = context.Message.Id,
+					Document = context.Message.Document
+				};
+				await _documentRepository.UploadAsync(documentDto, default(CancellationToken));
+			}
+
+		}
+	}
+}
